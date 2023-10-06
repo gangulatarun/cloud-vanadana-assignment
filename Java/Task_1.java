38565db6cae196d33d232185dc5ae78074afb283
gangulatarun <68196608+gangulatarun@users.noreply.github.com>
2023-10-06 03:05:39 -0700

    Update Task_1.java

--- Java/Task_1.java
+++ Java/Task_1.java
@@ -5,19 +5,17 @@ import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-        // Create array
+        
         Integer[] myArray = {1, 2, 3, 4, 5, 6, 7};
 
-        // Convert array to list for shuffling
+        
         List<Integer> list = Arrays.asList(myArray);
 
-        // Shuffle the list
         Collections.shuffle(list);
 
-        // Convert list back to array
         myArray = list.toArray(new Integer[0]);
 
-        // Print shuffled array
+        
         System.out.println(Arrays.toString(myArray));
     }
 }
